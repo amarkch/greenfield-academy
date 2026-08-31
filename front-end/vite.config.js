a6c8a1d625e31e3a41ce3d047f097fdd1f430570
AmarKch1990 <amarkch1990@gmail.com>
2026-08-31 14:46:18 +0530

    Adding Allowed host

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ["greenfield-academy.onrender.com", "greenfieldttb.com"],
     host: '0.0.0.0',
     port: process.env.PORT || 4000,
   },
