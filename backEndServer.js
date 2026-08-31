8a40608b625f5964e8f1299fed78762d135127db
AmarKch1990 <amarkch1990@gmail.com>
2026-08-31 22:38:37 +0530

    ccross origin fix - package install

--- backEndServer.js
+++ backEndServer.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const DATA_FILE = path.join(__dirname, 'data.json');
