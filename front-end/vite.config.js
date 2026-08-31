af392034857d0355664a14fee999be5237e6e738
AmarKch1990 <amarkch1990@gmail.com>
2026-08-31 14:43:43 +0530

    Adding host ip

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT,
+    host: '0.0.0.0',
+    port: process.env.PORT || 4000,
   },
 });
